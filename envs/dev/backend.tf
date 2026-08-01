4dc5f838de6afa56535944bae0c389ad1cd8fe6b
Ratnadweep <66510128+Ratnadweep@users.noreply.github.com>
2026-08-01 18:30:07 +0530

    Update backend.tf
    bucket = zen-pharma-terraform-state-ratnadweep-dpp-2026-zen-infra

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -2,7 +2,7 @@
 
 terraform {
   backend "s3" {
-    bucket       = "zen-pharma-terraform-state-ranjiniganeshan"
+    bucket       = "zen-pharma-terraform-state-ratnadweep-dpp-2026-zen-infra"
     key          = "envs/dev/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
